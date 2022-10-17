18ec1257eb7a07c2d22da0987001010f2e710054
Parag07c <choudharyparag001@gmail.com>
2022-10-17 22:22:03 +0530

    my first api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,7 @@ router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
     res.send('My second ever api!')
+
 });
 
 router.get('/students', function (req, res){
@@ -38,6 +39,63 @@ router.get('/student-details/:name', function(req, res){
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
+
 })
+router.get('/movies', function(req,res){
+    let arra=['Spider-man','Bahubali','M.S Dhoni the untold story']
+    res.send("The movies are :"+ arra);
+})
+
+router.get('/movies/:indexNumber',function(req,res){
+    let movies = ['P.K','3-idiots','Bahubali','Singham']
+    let movieName= req.params
+    res.send("The movie Name is :"+ movies[movieName.indexNumber])
+})
+router.get('/movies/:indexNumber',function(req,res){
+    let movies = ['P.K','3-idiots','Bahubali','Singham']
+    let movieName= req.params
+    res.send(movieName.indexNumber<movies.length?"The movie Name is :"+ movies[movieName.indexNumber]:"error: Please use a valid index")
+})
+router.get('/films',function(req,res){
+    let arrayofObj =[
+         {
+        "id":1,
+        "name":"The Shining"},
+        {
+            "id":2,
+            "name":"Incendies"},
+            {
+                "id":3,
+                "name":"Rang de Basanti"},
+                {
+                    "id":4,
+                    "name":"Finding Nemo"}
+         ]
+         res.send(arrayofObj)
+})
+router.get('/films/:filmId',function(req,res){
+    let arrayofObj =[
+         {
+        "id":1,
+        "name":"The Shining"},
+        {
+            "id":2,
+            "name":"Incendies"},
+            {
+                "id":3,
+                "name":"Rang de Basanti"},
+                {
+                    "id":4,
+                    "name":"Finding Nemo"}
+         ]
+         for(let i=0;i<arrayofObj.length;i++){
+            if(arrayofObj[i].id==req.params.filmId){
+                res.send(arrayofObj[i])
+                break;
+            }
+         }
+         res.send("error : No movie exists with this id")
+})
+
 
 module.exports = router;
