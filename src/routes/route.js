0da65d1a33904857e5f787a068dbbb07e5768c0b
Parag07c <choudharyparag001@gmail.com>
2022-10-19 22:07:32 +0530

    assignment of player data

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,43 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+let players =
+   [
+       {
+           name: "manish",
+           dob: "1/1/1995",
+           gender: "male",
+           city: "jalandhar",
+           sports: [
+               "swimming"
+           ]
+       },
+       {
+           name: "gopal",
+           dob: "1/09/1995",
+           gender: "male",
+           city: "delhi",
+           sports: [
+               "soccer"
+           ]
+       },
+       {
+           name: "lokesh",
+           dob: "1/1/1990",
+           gender: "male",
+           city: "mumbai",
+           sports: [
+               "soccer"
+           ]
+       },
+   ]
+   router.post("/playerData", function(req, res) {
+    let newPlayer=req.body
+    let arra=players.filter(elm=>elm.name==newPlayer.name)
+    arra.length==0?players.push(newPlayer):
+
+    res.send(  { data:players , status: true }  )
+})
+
+
 module.exports = router;
