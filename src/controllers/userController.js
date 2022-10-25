180aee51d8bec8ab503a22c74f0e6faa6dfea298
Parag07c <choudharyparag001@gmail.com>
2022-10-25 21:20:24 +0530

    Assignment Books collections

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,26 @@
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
+
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
+const createBooksData= async function(req,res){
+    let data=req.body
+    let savedBooksData=await UserModel.create(data)
+    res.send({message:savedBooksData})
+}
+const getBooksData= async function(req,res){
+    let allBooksData=await UserModel.find()
+    res.send({message:allBooksData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createBooksData=createBooksData
+module.exports.getBooksData=getBooksData
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.get('/getBooks',UserController.getBooksData)
+router.post('/addBooks',UserController.createBooksData)
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
 
 module.exports = router;
